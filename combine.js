ef234b598ddb60c32ceb0a68a291d929bea38470
Alena <alena10586@gmail.com>
2017-09-28 23:10:53 +0300

    HomeWork#1

---
+++ combine.js
@@ -0,0 +1,21 @@
+const fs   = require('fs');
+const path = require('path');
+
+const homework = './hlam';
+const ext      = '.css';
+const combine  = 'app.css';
+
+const streamWrite = fs.createWriteStream(combine);
+let stream;
+
+fs.readdirSync(homework).forEach(file => {
+    if (path.extname(file) === ext){
+        stream = fs.createReadStream(homework+'/'+file);
+        stream.pipe(streamWrite,{end:false});
+
+    }    
+});
+
+stream.on('end',() => {
+    streamWrite.end('JoyCasino.com');
+});
